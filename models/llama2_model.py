7314782f1adce98595aa86abad25e7cf2653c4e6
lpt <chut1987@gmail.com>
2024-07-13 10:19:27 +0800

    Add files via upload

--- models/llama2_model.py
+++ models/llama2_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # vim: set bg=dark noet ts=4 sw=4 fdm=indent :
 
-""" Wrapper of azure openai model"""
+""" Wrapper of llama2 model"""
 __author__ = 'linpingta'
---
+++ models/openai_model.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# vim: set bg=dark noet ts=4 sw=4 fdm=indent :
+
+""" Wrapper of openai model"""
+__author__ = 'linpingta'
+
+import os
+import openai
+
+# 默认的GPT配置
+DEFAULT_GPT_CONFIG = {
+    "engine": "devgpt4-32k",
+    "temperature": 0.0,
+    "max_tokens": 5000,
+    "top_p": 1.0,
+    "frequency_penalty": 0.0,
+    "presence_penalty": 0.0,
+    "stop": None
+}
+
+
+class OpenAIModel(object):
+    def __init__(self,
+                 api_config=DEFAULT_GPT_CONFIG,
+                 openai_api_key="",
+                 openai_api_base="",
+                 openai_api_version="",
+                 system_message=""):
+        # set api key
+        openai.api_key = openai_api_key
+        openai.api_base = openai_api_base
+        openai.api_version = openai_api_version
+
+        self.api_config = api_config
+        self.system_message = system_message
+        self.completion_tokens = 0
+        self.prompt_tokens = 0
+
+    def run(self, prompt, n=1, system_message="", logger=None):
+        try:
+            logger.info("start api request")
+
+            # system message if needed
+            sys_m = system_message if system_message else self.system_message
+            messages = [{"role": "system", "content": sys_m}, {"role": "user", "content": prompt}] if sys_m else [
+                {"role": "user", "content": prompt}]
+
+            text_outputs = []
+            raw_responses = []
+            while n > 0:
+                cnt = min(n, 10)
+                n -= cnt
+                res = openai.ChatCompletion.create(messages=messages, n=cnt, **self.api_config)
+                text_outputs.extend(choice["message"]["content"] for choice in res["choices"])
+
+                # add log into raw response
+                res['prompt'] = prompt
+                if sys_m:
+                    res['system_message'] = sys_m
+                raw_responses.append(res)
+
+                # log token usage
+                self.completion_tokens += res["usage"]["completion_tokens"]
+                self.prompt_tokens += res["usage"]["prompt_tokens"]
+
+            return text_outputs, raw_responses
+        except Exception as e:
+            logger.exception("An error occurred:", e)
+            return [], []
