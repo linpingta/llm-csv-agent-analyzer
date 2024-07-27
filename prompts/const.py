beba786bd5aec87187b408eddd4f2b523052817d
lpt <chut1987@gmail.com>
2024-07-27 19:12:44 +0800

    Add files via upload

---
+++ prompts/const.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# vim: set bg=dark noet ts=4 sw=4 fdm=indent :
+
+""" Main file for llm csv analyzer"""
+__author__ = 'linpingta'
+
+DEFAULT_PROMPT = 'assume you are an AI assistant,  please help to answer the following questions.'
